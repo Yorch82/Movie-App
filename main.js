57b9d19051ccb915014db4345b2000a29dd9de48
Yorch82 <jcampo1982@outlook.com>
2022-05-13 18:16:32 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+const API_KEY = 'api_key=222bd31d740eac11a8be3961350d67ab';
+const URL_INDEX = 'https://api.themoviedb.org/3';
+const API_URL = URL_INDEX + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
